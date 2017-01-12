e85b93e8db358efa6b75726bd0528b595b0e6381
Nikolay <nikecha96@gmail.com>
2017-01-11 22:16:05 -0800

    New speed control method
    Changed the way cruise speed is controlled to a more convenient one.
    Instead of manually have to press a button for each 5m/s stem, now
    pressing a button increases or decreases the target speed untill that
    button is pressed again.

--- CruiseControlScript.cs
+++ CruiseControlScript.cs
@@ -1,26 +1,35 @@
 const string INCREASE_SPEED_ARGUMENT = "+speed"; 
 const string DECREASE_SPEED_ARGUMENT = "-speed"; 
 const string TOGGLE_CRUISE_ARGUMENT = "-toggleCruise";
+const string PRECISION_MODE_ARGUMENT = "-precisionMode";
 
+const string DEBUG_PANEL_NAME = "[DEBUG] Text Panel";
 const string STATUS_PANEL_NAME = "[1] Text panel L";
 const string CONTROLLING_TIMER_NAME = "[1] Timer Cruise Control";	//name of the timer that runs this programmable block and triggers itself
 
-const int DEF_DECIMAL_PLACES = 2;
-const float DEF_SPEED_INCREMENT = 5.0F;
+const short DEF_DECIMAL_PLACES = 2;
+const float TARGET_SPEED_INCREMENT_PER_SECOND = 20.0F;	//The rate at which target speed is changed
 const float MAX_SPEED = 100.0F; 		//The maximum speed of the world the script is going to be used in
 								//(Or the maximum speed the user wants the ship to move with)
 const float MAX_SPEED_DEVIATION = 0.5F; //The maximum deviation that the script will allow between the 
                                                                             //target speed and the actual speed 
+const float PRECISION_FACTOR = 0.2F;	//DO NOT SET TO ZERO
+				//The precision of factor determines how much slower speed control is under precision mode. 0.1 means 10 times slower
 const string FORWARD_THRUSTERS_TAG = "(Forward)"; 
 const string BACKWARD_THRUSTERS_TAG = "(Backward)"; 
  
 Vector3D gGlobalGridPosition = new Vector3D(0,0,0); 
 float gCurrentSpeed = 0.0F;  
 float gTargetSpeed;   //The speed which the ship is supposed to cruise with. 
+float gTargetSpeedChangePerSecond = 0.0F;	//Current rate of change of the target speed per second
 short gCurrentAcceleration = 0;  // State of acceleration (-1 to decelerate, 1 to accelerate, 0 for neither) 
 bool gIsCruising = false;	//true if in cruise mode. False otherwise
+bool gPrecisionMode = false;	//true if precision mode is on. Precision mode allows for a precise speed control
 IMyTextPanel gStatusTextPanel;
 IMyTimerBlock gControlTimer;
+
+//Debug variables
+IMyTextPanel gDebugTextPanel;
 
 
 
@@ -30,6 +39,8 @@ public Program() {
     //===GETTING BLOCKS THAT WILL BE USED IN THE SCRIPT===
     gStatusTextPanel = (IMyTextPanel) GridTerminalSystem.GetBlockWithName(STATUS_PANEL_NAME);
     gControlTimer = (IMyTimerBlock) GridTerminalSystem.GetBlockWithName(CONTROLLING_TIMER_NAME);
+
+    gDebugTextPanel = (IMyTextPanel) GridTerminalSystem.GetBlockWithName(DEBUG_PANEL_NAME);
 }
 public void Main(string argument) {
 
@@ -38,6 +49,9 @@ public void Main(string argument) {
 	}
 	if(!gIsCruising) return;
 
+	if(argument == PRECISION_MODE_ARGUMENT) {
+		TogglePrecision();
+	}
 	if (argument == INCREASE_SPEED_ARGUMENT) {
 		increaseTargetSpeed();
 	}   
@@ -45,7 +59,7 @@ public void Main(string argument) {
 		decreaseTargetSpeed();
 	}
 
-
+	UpdateTargetSpeed();
     gCurrentSpeed = (float) getCurrentSpeed(DEF_DECIMAL_PLACES);  
  
     //===ALLIGNING THE CURRENT SPEED TO THE TARGET SPEED=== 
@@ -62,9 +76,11 @@ public void Main(string argument) {
     }
  
     //===WRITING TO TEXT PANEL===  
-    string textToWrite = "CurrentSpeed: " + gCurrentSpeed.ToString() + "\n"; 
-    textToWrite += "TargetSpeed: " + gTargetSpeed + "\n"; 
+    string textToWrite = "CurrentSpeed: " + Math.Round(gCurrentSpeed,1).ToString() + "\n"; 
+    textToWrite += "TargetSpeed: " + Math.Round(gTargetSpeed,1).ToString() + "\n";
+    if(gPrecisionMode) textToWrite += "Precision Mode...\n";
     gStatusTextPanel.WritePublicText(textToWrite);
+
 } 
 
 
@@ -82,7 +98,7 @@ double getCurrentSpeed(int decimalPlaces = DEF_DECIMAL_PLACES) {
          
     double speed = ((currentGlobalGridPosition-gGlobalGridPosition)*60).Length(); // how far the PB has moved since the last run (1/60s ago)             
     gGlobalGridPosition= currentGlobalGridPosition; // update the global variable, which will be used on the next run             
-    speed = Math.Round(speed,decimalPlaces); 
+    //speed = Math.Round(speed,decimalPlaces); //rounding the speed
      
     return speed;             
 }   
@@ -117,28 +133,36 @@ Overrides thrusters if doOverride = true, and removes override otherwise. Only o
 	}
 }
  
-void increaseTargetSpeed(float speedIncrement = -1.0F) { 
-	if(speedIncrement < 0) {	//If the speed increment passed is less than zero, this function chooses the speed increment
-		speedIncrement = (gTargetSpeed / 4) + 1;
+void increaseTargetSpeed() { 
+	if(gTargetSpeedChangePerSecond == 0.0F) {	//If the target speed is currently not being changed. In this case, this function should start increasing it
+		gTargetSpeedChangePerSecond = TARGET_SPEED_INCREMENT_PER_SECOND;
+		if(gPrecisionMode) gTargetSpeedChangePerSecond *= PRECISION_FACTOR;
 	}
-
-    float newSpeed = gTargetSpeed + speedIncrement; 
-    if (newSpeed > MAX_SPEED) newSpeed = MAX_SPEED;
-	newSpeed = (float)Math.Round(newSpeed,0);
-    gTargetSpeed = newSpeed; 
-    return; 
+	else if(gTargetSpeedChangePerSecond > 0.0) {	//If the target speed is currently being changed. In this case, this function should stop increasing it
+		gTargetSpeedChangePerSecond = 0.0F;
+	}
+	
 } 
 void decreaseTargetSpeed(float speedDecrement = -1.0F) {
-	if(speedDecrement < 0) {	//If the speed decrement passed is less than zero, this function chooses the speed decrement
-		speedDecrement = (gTargetSpeed / 4) + 1;
+	if(gTargetSpeedChangePerSecond == 0.0F) {	//If the target speed is currently not being changed. In this case, this function should start increasing it
+		gTargetSpeedChangePerSecond = -TARGET_SPEED_INCREMENT_PER_SECOND;
+		if(gPrecisionMode) gTargetSpeedChangePerSecond *= PRECISION_FACTOR;
 	}
-
-    float newSpeed = gTargetSpeed - speedDecrement; 
-    if (newSpeed < 0) newSpeed = 0;
-    newSpeed = (float)Math.Round(newSpeed,0);
-    gTargetSpeed = newSpeed;
-    return; 
+	else if(gTargetSpeedChangePerSecond < 0.0) {	//If the target speed is currently being changed. In this case, this function should stop increasing it
+		gTargetSpeedChangePerSecond = 0.0F;
+	}
 } 
+void UpdateTargetSpeed() {
+	gTargetSpeed += gTargetSpeedChangePerSecond / 60;	//TODO: Accomodate for the possibility of simulation speeds different than 60fps
+
+	if(gTargetSpeed > MAX_SPEED) {gTargetSpeed = MAX_SPEED; gTargetSpeedChangePerSecond = 0.0F;}
+	else if(gTargetSpeed < 0) {gTargetSpeed = 0; gTargetSpeedChangePerSecond = 0.0F;}
+}
+void TogglePrecision() {
+	gPrecisionMode = !gPrecisionMode;
+	if(gPrecisionMode) gTargetSpeedChangePerSecond *= PRECISION_FACTOR;
+	else gTargetSpeedChangePerSecond /= PRECISION_FACTOR;
+}
 void toggleCruise() {
 	gIsCruising = !gIsCruising;
 
